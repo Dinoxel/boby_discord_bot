a8b249b33d2f0dcf1cb4c6bef778e0f04658b114
Axel <simaxel@live.fr>
2023-02-22 10:39:29 +0100

    add helper and ticket commands

--- src/bot.py
+++ src/bot.py
@@ -1,18 +1,48 @@
 import discord
 from discord.ext import commands
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+import os
 
+load_dotenv(find_dotenv())
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix='>', intents=discord.Intents.all())
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+
+def lb(text):
+    """Add line break to the end of the text"""
+    return text + "\n"
 
 
 @bot.command()
-async def hello_world(context):
-    await context.send("Hello Boby!")
+async def helper(context):
+    return_text = lb("Available commands:")
+    available_commands = {"ticket": "ticket [-d, --details] <ticket_id>...",
+                          "helper": "helper"}
+
+    for command, detail in available_commands.items():
+        return_text += lb(f"`{command}` - {detail}")
+
+    await context.send(return_text)
+
+
+@bot.command()
+async def ticket(context, *tickets):
+    return_text = ""
+
+    tickets = tuple(set(tickets))
+    for ticket_id in tickets:
+        if ticket_id in ("-d", "--details"):
+            return_text += lb("details")
+            continue
+
+        return_text += lb(f"BOBY-{ticket_id}")
+
+    await context.send(return_text)
 
 
 if __name__ == "__main__":
