c6c38d3e02e6fe53a5719ffc309bd6a1c0004d79
Axel <simaxel@live.fr>
2023-03-13 02:47:57 +0100

    single jira ticket now displays summary, improve bot_manager command, add excluded gitlab users handling, improve environment variables

--- src/bot.py
+++ src/bot.py
@@ -19,18 +19,30 @@ if platform.system() == 'Windows':
 load_dotenv(find_dotenv())
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = int(os.getenv("GUILD_ID"))
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+DISCORD_GUILD_ID = int(os.getenv("DISCORD_GUILD_ID"))
+DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+DISCORD_ADMIN_ROLE_ID = int(os.getenv("DISCORD_ADMIN_ROLE_ID"))
 
 GITLAB_TOKEN = os.getenv("GITLAB_TOKEN")
-GITLAB_REPO_URL = os.getenv("GITLAB_REPO_URL")
+GITLAB_GROUP_NAME = os.getenv("GITLAB_GROUP_NAME")
+GITLAB_PROJECT_NAME = os.getenv("GITLAB_PROJECT_NAME")
+GITLAB_REPO_URL = f"https://gitlab.com/{GITLAB_GROUP_NAME}/{GITLAB_PROJECT_NAME}/-"
+
 GITLAB_PROJECT_ID = os.getenv("GITLAB_PROJECT_ID")
 GITLAB_API_URL = f'https://gitlab.com/api/v4/projects/{GITLAB_PROJECT_ID}/merge_requests' \
                  f'?state=opened' \
                  f'&per_page=250' \
                  f'&private_token={GITLAB_TOKEN}'
 
-JIRA_URL = os.getenv("JIRA_URL")
+JIRA_MAIL = os.getenv("JIRA_MAIL")
+JIRA_TOKEN = os.getenv("JIRA_TOKEN")
+JIRA_PROJECT = os.getenv("JIRA_PROJECT")
+JIRA_APP_NAME = os.getenv("JIRA_APP_NAME")
+
+JIRA_URL = f"https://{JIRA_APP_NAME}.atlassian.net/browse/"
+JIRA_API_URL = f"https://{JIRA_APP_NAME}.atlassian.net/rest/api/3/search?jql=PROJECT = {JIRA_PROJECT} AND KEY IN"
+JIRA_AUTH = aiohttp.BasicAuth(JIRA_MAIL, JIRA_TOKEN)
+JIRA_HEADERS = {"Accept": "application/json", "Content-Type": "application/json"}
 
 IS_DEBUG_MODE = eval(os.getenv("IS_DEBUG_MODE", "False"))
 
@@ -43,6 +55,9 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=command_prefix, intents=intents)
 
 previous_last_merge_request_id = None
+
+gitlab_excluded_users = {"Cafeine42", "BonaventureEleonore", "guillaumeharari", "AlexSarrazin"}
+last_merge_request_users = {"Cafeine42", "BonaventureEleonore"}
 
 
 @bot.command(name="ticket", aliases=["t", "tickets", "jira", "j"])
@@ -65,26 +80,77 @@ async def display_jira_tickets(ctx, *tickets):
         await ctx.send(embed=embed)
 
 
-@bot.command(name="loop")
-async def loop_manager(ctx, command=None):
+@bot.command(name="manager", aliases=["m"])
+async def bot_manager(ctx, command=None, sub_command=None, *parameters):
     """
-    Manages the loop that check for new merge requests
+    Manages the bot
 
-    :param ctx: The context of the command
-    :param command: The command to execute
-        :restart, -r: Restarts the Merge Request loop
-        :stop, -s: Stops the Merge Request loop
+    :param ctx: The context of commands
+    :param command: The command to execute (gitlab, loop)
+    :param sub_command: The sub command to execute
+        :gitlab: Manages the Gitlab excluded users list for the Merge Request loop
+            :-a: Adds a user to the excluded users list
+            :-d: Removes a user from the excluded users list
+        :loop: Manages the Merge Request loop
+            :-r: Starts the Merge Request loop
+            :-s: Stops the Merge Request loop
+    :param parameters: The parameters of the sub command
     """
+
     if command is None:
-        await ctx.send("No command provided\nAvailable commands: -r restart, -s stop")
-    elif command in {"-r", "restart"}:
-        await ctx.send(f"Loop restarted")
-        last_merge_request_checker.start()
-    elif command in {"-s", "stop"}:
-        await ctx.send(f"Loop stopped")
-        last_merge_request_checker.cancel()
+        await ctx.send("No command provided.")
+
+    elif ctx.author.id != DISCORD_ADMIN_ROLE_ID:
+        await ctx.send("You are not allowed to use this command.")
+
+    elif command in {"-history", "-h"}:
+        if sub_command in {"-delete", "-d"}:
+            try:
+                if parameters:
+                    for message_id in parameters:
+                        message = await ctx.channel.fetch_message(message_id)
+                        print("Deleted:", message.id, message.content or message.embeds[0].title)
+                        await message.delete()
+                else:
+                    async for message in ctx.history(limit=30):
+                        if bot.user.id == message.author.id:
+                            print("Deleted:", message.id, message.content or message.embeds[0].title)
+                            await message.delete()
+                            break
+            except discord.errors.NotFound:
+                print("Message not found.")
+    elif command in {"-gitlab", "-g"}:
+        if sub_command is None:
+            await ctx.send(f"Excluded users:\n ⦁ " + '\n⦁ '.join(gitlab_excluded_users))
+
+        elif not parameters:
+            await ctx.send("No parameters provided.")
+
+        elif sub_command in {"-add", "-a"}:
+            for gitlab_pseudo in parameters:
+                gitlab_excluded_users.add(gitlab_pseudo)
+            await ctx.send("Added following users to merge requests checker list: " + ', '.join(parameters))
+
+        elif sub_command in {"-delete", "-d"}:
+            for gitlab_pseudo in parameters:
+                gitlab_excluded_users.discard(gitlab_pseudo)
+            await ctx.send("Removed following users to merge requests checker list: " + ', '.join(parameters))
+
+    elif command in {"-loop", "-l"}:
+        if sub_command is None:
+            await ctx.send(f"Loop status: {last_merge_request_checker.is_running()}")
+
+        elif sub_command in {"-start", "-on"}:
+            print("Starting last_merge_request_checker task loop...")
+            last_merge_request_checker.start()
+            await ctx.send(f"Loop started")
+
+        elif sub_command in {"-stop", "-off"}:
+            print("Stopping last_merge_request_checker task loop...")
+            last_merge_request_checker.stop()
+            await ctx.send(f"Loop stopped")
     else:
-        await ctx.send("Unknown command\nAvailable commands: -r restart, -s stop")
+        await ctx.send("Unknown command.")
 
 
 @bot.command(name="joke", aliases=["blague"])
@@ -132,13 +198,12 @@ async def list_merge_requests(ctx, *arguments):
         async with session.get(GITLAB_API_URL) as resp:
             global previous_last_merge_request_id
             if resp.status != 200:
-                print(f"{resp.status} Error while fetching data from Gitlab")
+                print(f"Error {resp.status} while fetching data from Gitlab")
                 return
 
-            unconcerned_users = {"Cafeine42", "BonaventureEleonore", "guillaumeharari", "AlexSarrazin"}
             merge_requests = await resp.json()
             merge_requests = sorted(
-                (mr for mr in merge_requests if mr["author"]["username"] not in unconcerned_users),
+                (mr for mr in merge_requests if mr["author"]["username"] not in gitlab_excluded_users),
                 key=lambda mr: mr["iid"])
 
             arguments = list(arguments)
@@ -150,7 +215,8 @@ async def list_merge_requests(ctx, *arguments):
                     merge_requests = [mr for mr in merge_requests if mr["has_conflicts"]]
                     no_mr_text += " with conflicts"
 
-                merge_requests = [mr for mr in merge_requests if (mr["target_branch"] in arguments if arguments else True)]
+                merge_requests = [mr for mr in merge_requests if
+                                  (mr["target_branch"] in arguments if arguments else True)]
                 if arguments:
                     no_mr_text += f" found on `{'`, `'.join(arguments)}`"
 
@@ -192,19 +258,19 @@ async def last_merge_request_checker():
         async with session.get(GITLAB_API_URL) as resp:
             global previous_last_merge_request_id
             if resp.status != 200:
-                print(f"{resp.status} Error while fetching data from Gitlab")
+                print(f"Error {resp.status} while fetching data from Gitlab")
                 return
             merge_requests = await resp.json()
 
             last_merge_request = max(
-                (mr for mr in merge_requests if mr["author"]["username"] not in {"Cafeine42", "BonaventureEleonore"}),
+                (mr for mr in merge_requests if mr["author"]["username"] not in last_merge_request_users),
                 key=lambda mr: mr["iid"])
 
             if previous_last_merge_request_id is None:
                 previous_last_merge_request_id = last_merge_request["id"]
                 return
 
-            if last_merge_request["id"] <= previous_last_merge_request_id:
+            if last_merge_request["id"] > previous_last_merge_request_id:
                 return
 
             previous_last_merge_request_id = last_merge_request["id"]
@@ -219,11 +285,11 @@ async def last_merge_request_checker():
                             value=last_merge_request["title"],
                             inline=False)
 
-            mr_jira_id = re.search(r"^BOBY-(\d+)", last_merge_request['source_branch'])
+            mr_jira_id = re.search(fr"^{JIRA_PROJECT}-(\d+)", last_merge_request['source_branch'])
             if mr_jira_id:
                 mr_jira_id = mr_jira_id.groups()[0]
                 embed.add_field(name="Lien Jira",
-                                value=f"[BOBY-{mr_jira_id}]({JIRA_URL}-{mr_jira_id})",
+                                value=f"[{JIRA_PROJECT}-{mr_jira_id}]({JIRA_URL}-{mr_jira_id})",
                                 inline=True)
 
             if last_merge_request['target_branch']:
@@ -242,7 +308,7 @@ async def last_merge_request_checker():
                     value="",
                     inline=True)
 
-            channel = bot.get_guild(GUILD_ID).get_channel(CHANNEL_ID)
+            channel = bot.get_guild(DISCORD_GUILD_ID).get_channel(DISCORD_CHANNEL_ID)
             await channel.send(embed=embed,
                                content=f"MR {GITLAB_REPO_URL}/merge_requests/{last_merge_request['iid']}")
 
@@ -258,23 +324,51 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    if message.author.id == DISCORD_ADMIN_ROLE_ID \
+            and re.search(fr"^{command_prefix}m(?:anager)? +-h(?:istory)? +-d(?:elete)?",
+                          message.content.strip()) is not None:
+        await message.add_reaction('✅')
+
     jira_commands = [display_jira_tickets.name] + display_jira_tickets.aliases
     jira_message_checker = "\\" + command_prefix + "(?:" + "|".join(jira_commands) + r") ([\d ]+)(?=\D|$)"
     jira_tickets = re.findall(jira_message_checker, message.content)
-    if jira_tickets:
+
+    if len(jira_tickets) == 1 and len(jira_tickets[0].split()) == 1:
+        jira_ticket = jira_tickets[0].strip()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{JIRA_API_URL} ({JIRA_PROJECT}-{jira_ticket})",
+                                   auth=JIRA_AUTH,
+                                   headers=JIRA_HEADERS) as jira_resp:
+                if jira_resp.status != 200:
+                    error_summary = await jira_resp.json()
+                    channel = bot.get_guild(message.guild.id).get_channel(message.channel.id)
+                    await channel.send(f"Error {jira_resp.status} while fetching data from Jira\n"
+                                       f"Message: {error_summary['errorMessages'][0]}")
+                    return
+
+                ticket_summary = await jira_resp.json()
+                ticket_summary = ticket_summary['issues'][0]['fields']['summary']
+
+                embed = discord.Embed(title=f"{JIRA_PROJECT}-{jira_ticket}", color=0x0052cc)
+                embed.add_field(name="", value=ticket_summary)
+
+    elif jira_tickets:
         embed = discord.Embed(title="Tickets Jira", color=0x0052cc)
         for group_num, tickets_group in enumerate(jira_tickets, 1):
-            tickets_group = tickets_group.strip().split(" ")
+            tickets_group = tickets_group.strip().split()
             unique_tickets = list(dict.fromkeys(tickets_group))
 
             embed.add_field(name=f"Groupe {group_num}" if len(jira_tickets) > 1 else "",
                             value="".join(
-                                f"⦁ [BOBY-{ticket_id}]({JIRA_URL}-{ticket_id})\n" for ticket_id in unique_tickets if
+                                f"⦁ [{JIRA_PROJECT}-{ticket_id}]({JIRA_URL}-{ticket_id})\n" for ticket_id in
+                                unique_tickets if
                                 ticket_id.isdigit()),
                             inline=True)
+    else:
+        return
 
-        channel = bot.get_guild(message.guild.id).get_channel(message.channel.id)
-        await channel.send(embed=embed)
+    channel = bot.get_guild(message.guild.id).get_channel(message.channel.id)
+    await channel.send(embed=embed)
 
 
 if __name__ == "__main__":
